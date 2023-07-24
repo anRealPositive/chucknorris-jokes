48e1fc6771f8abf9c0d8f8c33c584ac99bc63670
Andrew Positko <positkoandrew@gmail.com>
2023-07-24 13:26:33 +0300

    WIP: wrap app with JokesProvider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+// libs
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// components
 import App from './App';
+
+// context
+import { JokesProvider } from './context/JokesProvider'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <>
+        <JokesProvider>
+            <App />
+        </JokesProvider>
+    </>
 );
