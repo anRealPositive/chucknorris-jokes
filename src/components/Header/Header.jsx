99a20f25e1f32caeb862508b705b08277508c80d
Andrew Positko <positkoandrew@gmail.com>
2023-07-24 12:31:58 +0300

    WIP: add AppBar with tabs to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,7 +22,33 @@ const Header = ({ title }) => {
                 className={styles.title}
             >
                 {title}
-            </Typography> 
+            </Typography>
+            <AppBar
+                color='default'
+                className={styles.appBar}
+                style={{ backgroundColor: 'rgb(19, 47, 76)' }}
+            >
+                <Tabs
+                    value={'Tab'}
+                    onChange={(e) => e}
+                    centered
+                >
+                <Tab
+                    label='all jokes'
+                    id='all-tab'
+                    className={styles.tab}
+                    sx={{ color: 'white' }}
+                    aria-controls='all-panel'
+                />
+                <Tab
+                    label='favourite'
+                    id='favourite-tab'
+                    className={styles.tab}
+                    sx={{ color: 'white' }}
+                    aria-controls='favourite-panel'
+                />
+                </Tabs>
+            </AppBar>
         </>
     );
 };
