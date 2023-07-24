bef1b0452265f3d32dff353f8cb556d9ec2fc1fa
Andrew Positko <positkoandrew@gmail.com>
2023-07-24 13:32:53 +0300

    add Loader to the app

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,35 @@
 // libs
-import React from 'react';
+import React, { useContext } from 'react';
 import { CssBaseline, Container } from '@mui/material';
 
 // components
 import Header from '../Header';
+import JokesSection from '../JokesSection';
+import Loader from '../Loader';
+
+// context
+import { JokesContext } from '../../context/JokesProvider';
 
 // styles
 import styles from './styles.module.scss';
-import JokesSection from '../JokesSection';
+
+/**
+ * Renders Header and Joke Sections
+ * @returns {JSX.Element}
+ */
 
 const Layout = () => {
+    const { 
+        isLoading,
+        error,
+    } = useContext(JokesContext);
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    };
+
+    if (isLoading) return <Loader />;
+
     return (
         <div className={styles.root}>
             <CssBaseline />
